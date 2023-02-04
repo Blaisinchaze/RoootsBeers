e9bd3f08d2497363eabdf92df5316eb61bcc48ae
Wojtek <wojciech.chmielewski@tidygames.co.uk>
2023-02-04 14:46:21 +0000

    set up player state machine and aim WIP

--- Assets/MainCharacterController.cs
+++ Assets/MainCharacterController.cs
@@ -13,11 +13,12 @@ public class MainCharacterController : MonoBehaviour
         AIRBORNE,
         AIMING
     }
-
-    Vector3 moveDirection;
+    Vector2 rawMovementInput;
+    Vector3 groundMoveDirection;
     Vector3 lookDirection;
     public Rigidbody rb;
-    public float movementSpeed = 5f;
+    public float groundMovementSpeed = 5f;
+    PlayerStates currentPlayerState = PlayerStates.GROUNDED;
     public ParticleSystem fizzPS;
     public Transform groundCheckTransform;
     public LayerMask ignoredColliders;
@@ -25,8 +26,11 @@ public class MainCharacterController : MonoBehaviour
 
     [SerializeField]bool isGrounded = true;
     public bool infiniteFizz = false;
-    bool requestedLaunch = false;
     bool isLaunching = false;
+
+    //areal controls properties
+    [Header("Aerial properties")]
+    public float airMovementSpeed = 2f;
 
     //Fizz variables
     [Header("FIZZ PROPERTIES")]
@@ -36,28 +40,144 @@ public class MainCharacterController : MonoBehaviour
     const float maxPossibleFizzValue = 1.0f;
     float currentMaxFizzValue = 0.5f;
     float currentFizzValue = 0f;
+    float maxExcitement = 1.0f;
+    float currentExcitement = 0.0f;
     [SerializeField] float fizzLaunchForce = 3f;
     float fizzFillPercent = 0.0f;
+
+    //Aim variables
+    [Header("AIM PROPERTIES")]
+    bool requestedAim = false;
+    bool isAiming = false;
 
     // Start is called before the first frame update
     void Start()
     {
         currentFizzValue = currentMaxFizzValue;
+
+    }
+
+
+
+    public void TransitionToState(PlayerStates newState)
+    {
+        PlayerStates tmpInitialState = currentPlayerState;
+        OnStateExit(tmpInitialState, newState);
+        currentPlayerState = newState;
+        OnStateEnter(newState, tmpInitialState);
+
+    }
+
+    private void OnStateEnter(PlayerStates state, PlayerStates fromState)
+    {
+        switch (state)
+        {
+            case PlayerStates.GROUNDED:
+                {
+
+                }
+                break;
+            case PlayerStates.AIRBORNE:
+                {
+
+                }
+                break;
+            case PlayerStates.AIMING:
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void OnStateExit(PlayerStates state, PlayerStates toState)
+    {
+        switch (state)
+        {
+            case PlayerStates.GROUNDED:
+                {
+
+                }
+                break;
+            case PlayerStates.AIRBORNE:
+                {
+
+                }
+                break;
+            case PlayerStates.AIMING:
+                {
+
+                }
+                break;
+            default:
+                break;
+        }
     }
 
     public void Update()
     {
-        //checking for Ground
-        isGrounded = Physics.Raycast(groundCheckTransform.position, -Vector3.up, 0.5f, ignoredColliders);
 
-        //Calculate percentage of fullness of the current fizz tank
-        fizzFillPercent = currentFizzValue / currentMaxFizzValue;
-        curveEvaluationValue = 1 - fizzFillPercent;
-        curveResultingValue = launchStrengthCurve.Evaluate(curveEvaluationValue);
+        #region STATE DEPENDANT LOGIC
+        //State changes
+        switch (currentPlayerState)
+        {
+            case PlayerStates.GROUNDED:
+                {
 
 
-        isLaunching = (requestedLaunch && currentFizzValue > 0);
+                    if (requestedAim && isGrounded)
+                    {
+                        TransitionToState(PlayerStates.AIMING);
+                    }
 
+
+                }
+                break;
+            case PlayerStates.AIRBORNE:
+                {
+                    if (currentFizzValue > 0)
+                    {
+                        isLaunching = true;
+                    }
+                    else
+                    {
+                        isLaunching = false;
+                    }
+                }
+                break;
+            case PlayerStates.AIMING:
+                {
+
+
+                    //Switch camera
+                    transform.Rotate(new Vector3(rawMovementInput.x, rawMovementInput.y, 0f));
+
+
+                    //Aim button released
+                    if (!requestedAim)
+                    {
+                        if (currentExcitement < 0.9f)
+                        {
+                            TransitionToState(PlayerStates.GROUNDED);
+                        }
+                    }
+
+                }
+                break;
+            default:
+                {
+
+                }
+                break;
+        }
+
+        #endregion
+
+        #region STATE INDEPENDANT LOGIC
+
+        //Handling fizz particle and fizz recharge based on current state
         if (!isLaunching)
         {
             fizzPS.Stop();
@@ -77,23 +197,58 @@ public class MainCharacterController : MonoBehaviour
             }
         }
 
+        //Excitement buildup when moving
+        if (rb.velocity.magnitude > 0 && !isLaunching)
+        {
+            currentExcitement += Time.deltaTime;
+        }
 
+        //Checking whether player is touchnig the ground
+        isGrounded = Physics.Raycast(groundCheckTransform.position, -Vector3.up, 0.5f, ignoredColliders);
+        if (currentPlayerState == PlayerStates.AIRBORNE && isGrounded && !isLaunching)
+        {
+            TransitionToState(PlayerStates.GROUNDED);
+        }
+
+        //Calculate percentage of fullness of the current fizz tank
+        fizzFillPercent = currentFizzValue / currentMaxFizzValue;
+        curveEvaluationValue = 1 - fizzFillPercent;
+        curveResultingValue = launchStrengthCurve.Evaluate(curveEvaluationValue);
+
+        #endregion
     }
-        //Read movement input
     public void UpdateMovement(InputAction.CallbackContext context)
     {
-        Vector2 vector2 = context.ReadValue<Vector2>();
-        moveDirection.x = vector2.x;
-        moveDirection.z = vector2.y;
+        rawMovementInput = context.ReadValue<Vector2>();
+        groundMoveDirection.x = rawMovementInput.x;
+        groundMoveDirection.z = rawMovementInput.y;
     }
-    public void UpdateLaunch(InputAction.CallbackContext context)
+
+    public void UpdateAim(InputAction.CallbackContext context)
     {
-         requestedLaunch = context.performed;
+        requestedAim = context.performed;
     }
 
     private void FixedUpdate()
     {
-       rb.MovePosition(rb.position + moveDirection * movementSpeed * Time.deltaTime);
+        //Player Movement when walking around
+        if (currentPlayerState == PlayerStates.GROUNDED && isGrounded)
+        {
+            rb.MovePosition(rb.position + groundMoveDirection * groundMovementSpeed * Time.deltaTime);
+        }
+
+        //Player movement when airborne
+        if (currentPlayerState == PlayerStates.AIRBORNE)
+        {
+            if (isLaunching)
+            {
+
+            }
+            else
+            {
+                rb.MovePosition(rb.position + groundMoveDirection * airMovementSpeed *  Time.deltaTime);
+            }
+        }
         if (isLaunching)
         {
             if (fizzFillPercent > 0.99)
