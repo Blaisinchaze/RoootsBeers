25be24f613e73d4b73b7e9e740a478a2962dd416
Quinn <Quinn.mcneil@tidygames.co.uk>
2023-02-04 11:25:16 +0000

    Added TMP

--- Assets/Scripts/Singletons/EventManager.cs
+++ Assets/Scripts/Singletons/EventManager.cs
@@ -1,8 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EventManager : Singleton<EventManager>
 {
-    
+    public UnityEvent GameStart = new();
+    public UnityEvent GameWin   = new();
+
+    public UnityEvent PlayerSpawn = new();
+    public UnityEvent PlayerDie   = new();
+
+    public BoolEvent SceneChange = new();
 }
+
+public class BoolEvent : UnityEvent<bool> { }
