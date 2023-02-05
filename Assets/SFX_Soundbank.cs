9d1b487c07dd9c2438132b1b4b1346cffea85cbe
Wojtek <wojciech.chmielewski@tidygames.co.uk>
2023-02-05 15:52:14 +0000

    Added audio assets and audio emitters

---
+++ Assets/SFX_Soundbank.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX_Soundbank : MonoBehaviour
+{
+    public static SFX_Soundbank instance;
+
+    public List<AudioClip> FizzSFX;
+    public List<AudioClip> BottleOpening;
+    public List<AudioClip> CarpetFootsteps;
+    public List<AudioClip> WoodFootsteps;
+    public List<AudioClip> BottleClinks;
+    public List<AudioClip> SloshingLiquid;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+}
